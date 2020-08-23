dc0e2a0835d5540b9a0eebbd38111ad92d4dacd8
Boris <zulu4@yandex.ru>
2020-08-24 00:49:04 +0500

    add human turn and move checks

--- src/ru/gb/TTT/TicTocToe.java
+++ src/ru/gb/TTT/TicTocToe.java
@@ -1,5 +1,7 @@
 package ru.gb.TTT;
 
+import java.util.Scanner;
+
 public class TicTocToe {
 
     static final char HUMAN_CELL = 'X';
@@ -13,12 +15,17 @@ public class TicTocToe {
 
         initGame();
         drawMap();
+        humanTurn();
+        drawMap();
+        humanTurn();
+        drawMap();
 
 
 
 
 
     }
+    //init
     public static void initGame() {
 
         for (int y=0;y<MAP_SIZE;y++) {
@@ -28,6 +35,7 @@ public class TicTocToe {
         }
         System.out.println("TicTocToe initiated"); //unnecessary remark
     }
+    //draw map
     public static void drawMap() {
         for (int y=0;y<MAP_SIZE;y++) {
             for (int x=0;x<MAP_SIZE;x++) {
@@ -36,6 +44,38 @@ public class TicTocToe {
             System.out.println();
         }
     }
+    //human turn
+    public static void humanTurn() {
+        int x=-1;
+        int y=-1;
+        System.out.println("Your turn, meatbag!");
+        do {
+            System.out.println("input X and Y");
+            Scanner sc = new Scanner(System.in);
+            if (sc.hasNextInt()) x = sc.nextInt();
+            if (sc.hasNextInt()) y = sc.nextInt();
+           } while (!isCellEmpty(x,y));
+        map[y - 1][x - 1] = HUMAN_CELL;
+    }
+
+    //check move: is cell empty? is cell in map? is input- integer?
+    public static boolean isCellInMap(int x, int y) {
+        if ( x<1 || x>MAP_SIZE || y<1 || y>MAP_SIZE ) {
+            return false;
+        }
+        return true;
+    }
+    public static boolean isCellEmpty(int x, int y) {
+        if (isCellInMap(x,y)) {
+            if (map[y - 1][x - 1] == EMPTY) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //check win
+    //ai turn
 
 
 }
